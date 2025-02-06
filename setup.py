d2d9150bb073f109c722568d98c814e73f31a460
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2025-02-06 12:10:44 +0900

    feat: scikit-learnをいれなおし

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
                 'prompt_toolkit',
                 'Pygments',
                 'sentence_transformers',
-                'sklearn',
+                'scikit-learn',
                 'setuptools',
                 'torch',
                 'tree_sitter',
